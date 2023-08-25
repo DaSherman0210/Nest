35fc70b21d6060b1a605ee47560a47b61fe9e501
DaSherman0210 <tgerman181@gmail.com>
2023-08-25 08:43:58 -0500

    fix: :bug: Bug fixed
    Fixed bug data was missing

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ParachutesModule } from './parachutes/parachutes.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AstronautasModule } from './astronautas/astronautas.module';
 
 @Module({
-  imports: [ ParachutesModule, AstronautasModule],
+  imports: [ ParachutesModule , MongooseModule.forRoot('mongodb+srv://astronautasUser:12345@astronautacluster.ucf6lip.mongodb.net/astronautasDB'), AstronautasModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
